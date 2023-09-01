fccee378e37b7876f9be159aec6218253387dfd2
NicolasSoulay <soulaynicolas@gmail.com>
2023-09-01 10:22:03 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { MapComponent } from './components/map/map.component';
+import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    MapComponent,
+    BookmarksComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
