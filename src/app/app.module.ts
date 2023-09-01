8a43f3a5e673d272085bad690cb84cdcf17f8681
NicolasSoulay <soulaynicolas@gmail.com>
2023-09-01 10:48:16 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { FormComponent } from './components/form/form.component';
 import { MapComponent } from './components/map/map.component';
 import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HistoryComponent } from './components/history/history.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { InscriptionFormComponent } from './components/inscription-form/inscription-form.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,9 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     MapComponent,
     BookmarksComponent,
     NavBarComponent,
+    HistoryComponent,
+    LoginFormComponent,
+    InscriptionFormComponent,
   ],
   imports: [
     BrowserModule
