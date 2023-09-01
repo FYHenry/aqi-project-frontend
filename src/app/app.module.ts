50815da2019e1900f5b900940f5171e671bb642d
NicolasSoulay <soulaynicolas@gmail.com>
2023-09-01 10:56:26 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FormComponent } from './components/form/form.component';
 import { MapComponent } from './components/map/map.component';
-import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { HistoryComponent } from './components/history/history.component';
-import { LoginFormComponent } from './components/login-form/login-form.component';
-import { InscriptionFormComponent } from './components/inscription-form/inscription-form.component';
+import { BookmarksComponent } from './components/map/bookmarks/bookmarks.component';
+import { NavBarComponent } from './components/map/nav-bar/nav-bar.component';
+import { HistoryComponent } from './components/map/history/history.component';
+import { LoginFormComponent } from './components/map/login-form/login-form.component';
+import { InscriptionFormComponent } from './components/map/inscription-form/inscription-form.component';
+import { ForumComponent } from './components/forum/forum.component';
+import { ThreadComponent } from './components/forum/thread/thread.component';
+import { HomeComponent } from './components/forum/home/home.component';
+import { BackOfficeComponent } from './components/back-office/back-office.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
     MapComponent,
     BookmarksComponent,
     NavBarComponent,
     HistoryComponent,
     LoginFormComponent,
     InscriptionFormComponent,
+    ForumComponent,
+    ThreadComponent,
+    HomeComponent,
+    BackOfficeComponent,
   ],
   imports: [
     BrowserModule
