b963bfc91528c2c8cb510bbc3f7741edf4ae4ce6
NicolasSoulay <soulaynicolas@gmail.com>
2023-09-01 10:26:25 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
+import { FormComponent } from './components/form/form.component';
 import { MapComponent } from './components/map/map.component';
 import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
