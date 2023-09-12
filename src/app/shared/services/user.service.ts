ffc24b945324a8fdbc49f2edf9dfe955cbf481d0
NicolasSoulay <soulaynicolas@gmail.com>
2023-09-12 17:58:47 +0200

    conflit

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -9,7 +9,7 @@ interface HttpResponseBody {
     date: string,
     code: string,
     message: string,
-    url: string 
+    url: string
 }
 
 @Injectable({
@@ -31,17 +31,10 @@ export class UserService{
         return this._http.get<User>(`${this._baseUrl}/${id}`);
     }
 
-    /*public create(created: NewUser){
-        console.log("UserService CREATE: ", this._baseUrl, created);
-        return this._http.post(this._baseUrl, created);
-    }*/
-    
     public create(created: NewUser): Observable<HttpResponseBody>{
         console.log("UserService NEW CREATE: ", this._baseUrl, created);
-        //return this._http.post<HttpResponseBody>(this._baseUrl, created, {observe: "body", responseType: "json"});
         return this._http.post<HttpResponseBody>(this._baseUrl, created);
     }
-
 
     public update(updated: User){
         console.log("UserService UPDATED: ", updated);
@@ -54,7 +47,7 @@ export class UserService{
         return this._http
             .put(`${this._baseUrl}/pwd/${updated.id}`, updated)
     }
-  
+
     public findConnectedUserById(id: number): Observable<ConnectedUser> {
         return this._http.get<ConnectedUser>(`${this._baseUrlRetrieve}/${id}`);
     }
